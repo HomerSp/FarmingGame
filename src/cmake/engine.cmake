48682e0b105e86624a56f97e1e84b6138ba9e0eb
Mathias Tillman <master.homer@gmail.com>
2018-11-06 03:51:07 +0100

    Build using c++14.

--- src/cmake/engine.cmake
+++ src/cmake/engine.cmake
@@ -1,4 +1,7 @@
+find_package(Threads)
+
 include_directories(
+    ${src_DIR}/chaiscript/include
     ${src_DIR}/jsoncpp/include
     ${src_DIR}/lodepng/src
     ${src_DIR}/engine/include
@@ -8,4 +11,5 @@ link_directories(${src_DIR}/jsoncpp)
 link_directories(${src_DIR}/lodepng)
 link_directories(${src_DIR}/engine)
 
-set(engine_LIBRARY engine jsoncpp_lib_static lodepng)
+set(engine_EXTRA_LIBRARY jsoncpp_lib_static lodepng ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_DL_LIBS})
+set(engine_LIBRARY engine ${engine_EXTRA_LIBRARY})
