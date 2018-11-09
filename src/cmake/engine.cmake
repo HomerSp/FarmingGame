cc18a25cc34b5576e580f03cd7575842ea937318
Mathias Tillman <master.homer@gmail.com>
2018-11-09 23:46:46 +0100

    Move external modules to their own subdirectory.

--- src/cmake/engine.cmake
+++ src/cmake/engine.cmake
@@ -1,14 +1,15 @@
 include_directories(
-    ${src_DIR}/angelscript/src/sdk/angelscript/include
-    ${src_DIR}/angelscript/src/sdk/add_on
-    ${src_DIR}/angelscript/addon/include
-    ${src_DIR}/jsoncpp/include
-    ${src_DIR}/lodepng/src
+    ${src_DIR}/external/angelscript/src/sdk/angelscript/include
+    ${src_DIR}/external/angelscript/src/sdk/add_on
+    ${src_DIR}/external/angelscript/addon/include
+    ${src_DIR}/external/jsoncpp/include
+    ${src_DIR}/external/lodepng/src
     ${src_DIR}/engine/include
 )
 
-link_directories(${src_DIR}/jsoncpp)
-link_directories(${src_DIR}/lodepng)
+link_directories(${src_DIR}/external/angelscript)
+link_directories(${src_DIR}/external/jsoncpp)
+link_directories(${src_DIR}/external/lodepng)
 link_directories(${src_DIR}/engine)
 
 set(engine_EXTRA_LIBRARY jsoncpp_lib_static lodepng)
