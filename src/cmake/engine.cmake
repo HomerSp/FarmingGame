d712d5c7a845c393629b67a9d1b25f0a9505fb6b
Mathias Tillman <master.homer@gmail.com>
2018-11-11 11:39:54 +0100

    Move angelscript functionptr addon to its own directory.

--- src/cmake/engine.cmake
+++ src/cmake/engine.cmake
@@ -1,7 +1,7 @@
 include_directories(
     ${src_DIR}/external/angelscript/src/sdk/angelscript/include
     ${src_DIR}/external/angelscript/src/sdk/add_on
-    ${src_DIR}/external/angelscript/addon/include
+    ${src_DIR}/angelscript/addon/include
     ${src_DIR}/external/jsoncpp/include
     ${src_DIR}/external/lodepng/src
     ${src_DIR}/engine/include
@@ -10,7 +10,8 @@ include_directories(
 link_directories(${src_DIR}/external/angelscript)
 link_directories(${src_DIR}/external/jsoncpp)
 link_directories(${src_DIR}/external/lodepng)
+link_directories(${src_DIR}/angelscript)
 link_directories(${src_DIR}/engine)
 
 set(engine_EXTRA_LIBRARY jsoncpp_lib_static lodepng)
-set(engine_LIBRARY engine angelscript angelscript_addon ${engine_EXTRA_LIBRARY})
+set(engine_LIBRARY engine angelscript angelscript_addon angelscript_function ${engine_EXTRA_LIBRARY})
